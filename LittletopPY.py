daacc06fe0bfae774fed1b6f6db391979d34078c
rich98 <51236157+rich98@users.noreply.github.com>
2024-02-25 17:45:10 +0000

    Update LittletopPY.py

--- LittletopPY.py
+++ LittletopPY.py
@@ -1,10 +1,12 @@
-#rich98 (c) GNU GENERAL PUBLIC LICENSE Version 3.
-
 import psutil
 import time
 import tkinter as tk
+import platform
 
-def print_top(root, text_widget):
+def print_top(text_widget):
+    # Clear the text widget before updating
+    text_widget.delete('1.0', tk.END)
+
     # Get CPU stats for each core
     cpu_percentages = psutil.cpu_percent(percpu=True)
     for i in range(0, len(cpu_percentages), 3):
@@ -25,15 +27,18 @@ def print_top(root, text_widget):
     text_widget.insert(tk.END, memory_stats)
 
     # Get operating system info
-    text_widget.insert(tk.END, "this scrpt may report Windows 11 as Windows 10. This is not a bug\n")
+    text_widget.insert(tk.END, "This script may report Windows 11 as Windows 10. This is not a bug.\n")
     text_widget.insert(tk.END, "answers.microsoft.com – Win11 Shows Win10 as OS\n")
     
     os_info = platform.uname()
     text_widget.insert(tk.END, f"Operating System: {os_info.system}, OS Release: {os_info.release}, OS Version: {os_info.version}\n\n") 
-    # Calculate the maximum length of the process names
-    max_name_length = max(len(p.info['name']) for p in processes[:150])
 
-    # Print the top 150 processes
+    # Get the top 25 processes
+    processes = sorted(psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']), key=lambda p: p.info['cpu_percent'], reverse=True)
+
+    # Calculate the maximum length of the process names
+    max_name_length = max(len(p.info['name']) for p in processes[:25])
+
     text_widget.insert(tk.END, "PID".ljust(8) + "NAME".ljust(max_name_length) + "CPU %".ljust(10) + "MEM USAGE (in MB)\n")
     for process in processes[:25]:
         mem_usage = process.info['memory_info'].rss / (1024 ** 2)  # Convert memory usage to MB
